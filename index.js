722be2cda41d01a7c8c532fcac7d7f0508b1e1ce
ITSZERO0 <aimenjabrani7@gmail.com>
2025-06-10 19:15:34 +0200

    index.js

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ async function startBumpLoop() {
       await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds
     }
 
-    // After all tokens have sent the bump command, wait 2 hours and 15 minutes (8100000ms)
-    console.log(`All accounts have sent the bump. Waiting 2 hours and 15 minutes before restarting...`);
-    await new Promise((resolve) => setTimeout(resolve, 8100000));
+    // After all tokens have sent the bump command, wait 2 hours and 5 minutes (7500000ms)
+    console.log(`All accounts have sent the bump. Waiting 2 hours and 5 minutes before restarting...`);
+    await new Promise((resolve) => setTimeout(resolve, 7500000));
   }
 }
 
